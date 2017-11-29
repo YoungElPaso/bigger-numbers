a23428c29840c26a6c2775e7687d9343ee82e9ca
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-11-29 15:00:08 -0500

    added import of BN tag and mounted using riot in app

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // Main file for bigger-numbers component.
+console.info(riot ? "RiotJS is loaded!" : null);
+
+// Import the compiled Bigger Numbers tag.
+import "./compiled-tags/bigger-numbers.js";
+
+// Mount RiotJS elements.
+riot.mount("*");
