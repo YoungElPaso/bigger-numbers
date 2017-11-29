1bebef438421a8d65e0ac8f04489d9db4fe21e33
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-11-29 16:09:54 -0500

    just mounting one component now

--- app.js
+++ app.js
@@ -4,5 +4,7 @@ console.info(riot ? "RiotJS is loaded!" : null);
 // Import the compiled Bigger Numbers tag.
 import "./compiled-tags/bigger-numbers.js";
 
-// Mount RiotJS elements.
-riot.mount("*");
+// Mount RiotJS elements. In this case just one, bigger-numbers.
+// TODO: make this conditional on some contexts.
+// I.e. don't do it on every page. But hey, this is just a demo!
+riot.mount("bigger-numbers");
