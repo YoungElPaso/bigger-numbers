a0f8efbd44271e20e046f4f8e7843ff01e238df2
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-11-30 02:04:57 -0500

    rebuilt bundle w/ subcomponents

--- bundle.js
+++ bundle.js
@@ -1,16 +1,25 @@
 (function () {
 'use strict';
 
-riot.tag2('bigger-numbers', '<div class="bignumber_wrapper {opts.styling}"> <div class="info" if="{opts.info}"> {opts.info} </div> <div class="bignumber_content"> {opts.preamble} <yield from="preamble"></yield> </div> <div class="bignumber_value"> {opts.value} <yield from="value"></yield> </div> <div class="bignumber_content"> {opts.caption} <yield from="caption"></yield> <yield></yield> </div> </div>', 'bigger-numbers .info,[data-is="bigger-numbers"] .info{ color: gray; font-size: small; } bigger-numbers .bignumber_wrapper,[data-is="bigger-numbers"] .bignumber_wrapper{ border: 1px solid; padding: 1em; } bigger-numbers .bignumber_wrapper.center,[data-is="bigger-numbers"] .bignumber_wrapper.center{ text-align: center; } bigger-numbers .bignumber_wrapper.even-bigger,[data-is="bigger-numbers"] .bignumber_wrapper.even-bigger{ font-size: xx-large; };', '', function(opts) {
+riot.tag2('biggernumbers', '<div class="bignumber_wrapper {opts.styling}"> <div class="info" if="{opts.info}"> {opts.info} </div> <div class="bignumber_content"> {opts.preamble} <yield from="preamble"></yield> </div> <div class="bignumber_value"> {opts.value} <yield from="value"></yield> </div> <div class="bignumber_content"> {opts.caption} <yield from="caption"></yield> <yield></yield> </div> </div>', 'biggernumbers .info,[data-is="biggernumbers"] .info{ color: gray; font-size: small; } biggernumbers .bignumber_wrapper,[data-is="biggernumbers"] .bignumber_wrapper{ border: 1px solid; padding: 1em; } biggernumbers .bignumber_wrapper.center,[data-is="biggernumbers"] .bignumber_wrapper.center{ text-align: center; } biggernumbers .bignumber_wrapper.even-bigger,[data-is="biggernumbers"] .bignumber_wrapper.even-bigger{ font-size: xx-large; };', '', function(opts) {
+});
+
+riot.tag2('biggernumbersvalue', '<span if="{opts.value}"> {opts.value} </span> <yield></yield>', '', '', function(opts) {
+});
+
+riot.tag2('biggernumberscaption', '<span if="{opts.caption}"> {opts.caption} </span> <yield></yield>', '', '', function(opts) {
+});
+
+riot.tag2('biggernumberspreamble', '<span if="{opts.preamble}"> {opts.preamble} </span> <yield></yield>', '', '', function(opts) {
 });
 
 // Main file for bigger-numbers component.
 console.info(riot ? "RiotJS is loaded!" : null);
 
-// Import the compiled Bigger Numbers tag.
+// Import the compiled Bigger Numbers tags.
 // Mount RiotJS elements. In this case just one, bigger-numbers.
 // TODO: make this conditional on some contexts.
 // I.e. don't do it on every page. But hey, this is just a demo!
-riot.mount("bigger-numbers");
+riot.mount("biggernumbers");
 
 }());
