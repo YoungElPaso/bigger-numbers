23df8a77ac6cd7734bfa41d50b63ce5b36d6c87a
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-11-30 02:04:35 -0500

    added a bunch of subcomponents, importing them in app for the bundle

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // Main file for bigger-numbers component.
 console.info(riot ? "RiotJS is loaded!" : null);
 
-// Import the compiled Bigger Numbers tag.
+// Import the compiled Bigger Numbers tags.
 import "./compiled-tags/bigger-numbers.js";
+import "./compiled-tags/bigger-numbers-value.js";
+import "./compiled-tags/bigger-numbers-caption.js";
+import "./compiled-tags/bigger-numbers-preamble.js";
 
 // Mount RiotJS elements. In this case just one, bigger-numbers.
 // TODO: make this conditional on some contexts.
 // I.e. don't do it on every page. But hey, this is just a demo!
-riot.mount("bigger-numbers");
+riot.mount("biggernumbers");
