1fef292bd5ed1440b49507b1cf43ddf795d0a787
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-11-30 11:06:56 -0500

    another demo, with data-is examples and slightly modified bundle

--- bundle.js
+++ bundle.js
@@ -17,9 +17,6 @@ riot.tag2('biggernumberspreamble', '<span if="{opts.preamble}"> {opts.preamble} 
 console.info(riot ? "RiotJS is loaded!" : null);
 
 // Import the compiled Bigger Numbers tags.
-// Mount RiotJS elements. In this case just one, bigger-numbers.
-// TODO: make this conditional on some contexts.
-// I.e. don't do it on every page. But hey, this is just a demo!
-riot.mount("biggernumbers");
+riot.mount("*");
 
 }());
