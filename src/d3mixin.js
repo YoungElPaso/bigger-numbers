b216b5d92629d1e3785eca233458f6bb0d3089c2
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-12-04 17:11:07 -0500

    actually addded working d3 to mixin, still need to make the import process better/figure out coupling

--- src/d3mixin.js
+++ src/d3mixin.js
@@ -1,9 +1,19 @@
 "use strict";
+
+// Bring D3 along for the ride.
+import * as d3 from "d3";
+
+// TODO: change depenendencies to JUST what d3 we want.
+// Ie Bring along *certain* d3 modules for the ride!
+// import { scaleLinear } from "d3-scale";
+
 let D3Mixin = {
   init: function() {
     this.on("mount", function() {
       // console.log("got the D3Mixin into the mix...");
-      console.log("Mounted!");
+      console.log("Mounted!", d3, this.root);
+      // D3ify something!
+      d3.select(this.root).style("background-color", "pink");
     });
   }
 };
