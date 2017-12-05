c820a3e170220e5e30aa5793dc22477583f9cf4b
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-12-05 14:35:53 -0500

    used some actual D3 fancy interpolation methods to do fancy stuff w/ the value

--- src/d3mixin.js
+++ src/d3mixin.js
@@ -13,7 +13,19 @@ let D3Mixin = {
       // console.log("got the D3Mixin into the mix...");
       console.log("Mounted!", d3, this.root);
       // D3ify something!
-      d3.select(this.root).style("background-color", "pink");
+      // Using interpolation to map range of values to a colour.
+      let range = 100;
+      let bgColor = d3.scaleSequential(d3.interpolateCool).domain([0, range]);
+      let color = d3.scaleSequential(d3.interpolateWarm).domain([0, range]);
+      let fontSize = d3.scaleSequential(d3.interpolateRound(0, range / 50));
+      let cValue = this.opts.value;
+      // Only works for a number value!
+      // If eValue isn't a number, let's assume something in the middle of
+      // the domain.
+      cValue = isNaN(cValue) ? 50 : cValue;
+      d3.select(this.root).style("background-color", bgColor(cValue));
+      d3.select(this.root).style("color", color(range - cValue));
+      d3.select(this.root).style("font-size", fontSize(cValue) + "px");
     });
   }
 };
