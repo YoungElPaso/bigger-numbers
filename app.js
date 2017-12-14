30af9124c6e578e6026f44e2736c57ce3e2d8146
Jesse Sutherland <jesse.a.sutherland@gmail.com>
2017-12-14 14:13:36 -0500

    added new simplified big-number tag and fallback tag

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ import "./compiled-tags/bigger-numbers-value.js";
 import "./compiled-tags/bigger-numbers-caption.js";
 import "./compiled-tags/bigger-numbers-preamble.js";
 
+// Import new compiled, simplified tag.
+import "./compiled-tags/big-number.js";
+// Import new generic fallback tag.
+import "./compiled-tags/fallback.js";
+
 // Mount RiotJS elements. In this case just one, bigger-numbers.
 // TODO: make this conditional on some contexts.
 // I.e. don't do it on every page. But hey, this is just a demo!
